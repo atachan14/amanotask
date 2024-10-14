68ec5b99a5e3a5bdc5066ab90e47e81c09f1c1b8
atachan14 <aklius14pg>
2024-10-15 08:34:54 +0900

    スコア関連の形はおわり！これからバグ取り！

--- src/chapter05_method/G03_poker.java
+++ src/chapter05_method/G03_poker.java
@@ -2,13 +2,13 @@ package chapter05_method;
 
 import java.util.Iterator;
 
-/* 予定中の制作順序。
- * 		スコアの保存,判定方法やルートかんがえちゅう～～。
- * 		各スコア取得メソッド
- * スコアを参照したディーラーのリロール判定メソッド
+/* 予定。
+ * ディーラーのリロール判定メソッド
  * result関係
  * リトライ関係
  * オプション（リロール数等）によるコイン倍率の変化
+ * リロールでのコイン
+ * score[1]関連	
  * pHandsとdHandsに分けてオプションとして追加
  * 
  */
@@ -20,16 +20,18 @@ public class G03_poker {
 		int handsMax = 5;
 		int pRerollMax = 3;
 		int dRerollMax = 3;
+		int dRerollLevel = 450;
 
 		int totalPersons = playerMax + dealerMax;
-		String[] playerName = new String[] { "p1", "p2", null, null };
+		String[] playerName = new String[] { "player1", "p2", null, null };
 		int[] deck = new int[13 * 4];
 		int[][] hands = new int[totalPersons][handsMax];
 		int[] rerollIndex = new int[handsMax];
 		int[] rerollStock = new int[totalPersons];
-		int[] conboScore = new int[totalPersons];
-		int[] numScore = new int[totalPersons];
+		int[] topPlayer = new int[totalPersons];
+		int[][] score = new int[totalPersons][2];
 		int[] usedIndex = new int[totalPersons]; // digitで保存
+		int[] winner = new int[totalPersons];
 		int[][] input = new int[totalPersons][2];// 0にインプット、1にflag(0:初期値,1:openingAnnounce済,2:hold)
 
 		for (int i = 0; i < playerMax; i++) {
@@ -77,8 +79,16 @@ public class G03_poker {
 //				dealerRerollJudge();
 			}
 		}
-//		result();
 
+		handsSort(hands);
+		for (int player = 0; player < totalPersons; player++)
+			scoreClearing(player, hands, score);
+		winnerJudge(score, winner);
+//		if (winner.length > 1) {
+//			drawResult();
+//		} else {
+//			result();
+//		}
 	}
 
 	public static void playerConf(String[] playerName, int playerMax, int totalPersons) {
@@ -110,11 +120,21 @@ public class G03_poker {
 	public static void playerAnnounse(int nowPlayer, String[] playerName) {
 		System.out.println();
 		System.out.println();
-		System.out.println(playerName[nowPlayer] + "'s turn");
+		System.out.println("	" + playerName[nowPlayer] + "'s turn");
 	}
 
 	public static void HandsDisplay(int nowPlayer, String[] playerName, int[][] hands) {
-		System.out.print(playerName[nowPlayer] + " ");
+//		System.out.print(playerName[nowPlayer] + " ");
+		System.out.print(" →　");
+		for (int i = 0; i < hands[0].length; i++) {
+			System.out.print("【" + convertCard(hands[nowPlayer][i]) + "】 ");
+		}
+		handsSort(hands);
+		System.out.println();
+//		for(int i = 0 ; i < playerName[nowPlayer].length()-1;i++) {
+//			System.out.print(" ");
+//		}
+		System.out.print("　 ");
 		for (int i = 0; i < hands[0].length; i++) {
 			System.out.print("【" + convertCard(hands[nowPlayer][i]) + "】 ");
 		}
@@ -133,6 +153,8 @@ public class G03_poker {
 // リロールの枚数とインデックスの最大値チェック
 // エラーが出たらインプットからループ
 	public static void inputAndErrorCheck(int nowPlayer, int hands[][], int[][] input) {
+		boolean length = true;
+		boolean size = true;
 		while (true) {
 			input[nowPlayer][1] = new java.util.Scanner(System.in).nextInt();
 			if (input[nowPlayer][1] == 0) {
@@ -141,17 +163,24 @@ public class G03_poker {
 			}
 			int temp = input[nowPlayer][1];
 			int digit = 0;
+			
+			length = true;
+			size = true;
+			
 			while (temp != 0) {
-				if (temp % 10 > hands[nowPlayer].length)
-					System.out.print(temp + "は無効です。　＞");
+				if (temp % 10 > hands[nowPlayer].length) {
+					System.out.print(temp%10 + "は無効です。　＞");
+					length = false;
+				}
 				digit++;
 				temp /= 10;
+				if (digit > hands[nowPlayer].length) {
+					System.out.print(digit - hands[nowPlayer].length + "桁多いです。 ＞");
+					size = false;
+				}
+				if (length&& size )
+					return;
 			}
-			if (digit > hands[nowPlayer].length)
-				System.out.print(digit - hands[nowPlayer].length + "桁多いです。 ＞");
-
-			if (digit <= hands[nowPlayer].length)
-				return;
 		}
 	}
 
@@ -172,12 +201,14 @@ public class G03_poker {
 		}
 	}
 
-	public static void dealerRerollJudge(int nowPlayer, String[] playerName, int[][] hands, int[] rerollStock,
-			int[] deck) {
-//		if (score[nowPlayer] < dealerJudge) {
-
+//	public static void dealerRerollJudge(int nowPlayer, int[][] hands, int[] rerollStock,int dRerollLevel,int[][] score) {
+//		score[nowPlayer]
+//		if (score[nowPlayer][0] < dRerollLevel) {
+//			
 //		}
-	}
+//
+////		}
+//	}
 
 // カードを引く前にデッキを確認するメソッド。
 	public static int tempDraw(int[] deck) {
@@ -231,6 +262,21 @@ public class G03_poker {
 		return convertedDigit;
 	}
 
+	// ストレート系判定用
+	public static void handsSort(int[][] hands) {
+		for (int p = 0; p < hands.length; p++) {
+			for (int i = 0; i < hands[p].length - 1; i++) {
+				for (int j = i + 1; j < hands[p].length; j++) {
+					if (hands[p][i] % 13 < hands[p][j] % 13) {
+						int temp = hands[p][i];
+						hands[p][i] = hands[p][j];
+						hands[p][j] = temp;
+					}
+				}
+			}
+		}
+	}
+
 	public static void arraySort(int[] num) {
 		for (int i = 0; i < num.length - 1; i++) {
 			for (int j = i + 1; j < num.length; j++) {
@@ -243,66 +289,98 @@ public class G03_poker {
 		}
 	}
 
-	public static void arrayLeftSort(int[][] score) {
-		for (int i = 0; i < score.length - 1; i++) {
-			for (int j = i + 1; j < score.length; j++) {
-				if (score[i][0] < score[j][0]) {
-					int temp = score[i][0];
-					score[i][0] = score[j][0];
-					score[j][0] = temp;
+	// 1位を探すメソッド
+	public static void winnerJudge(int[][] score, int[] winner) {
+		int maxScore = 0;
+		int tied = 0;
+		for (int i = 0; i < score.length; i++) {
+			if (maxScore < score[i][0]) {
+				maxScore = score[i][0];
+				winner[0] = i;
+				for (int j = 1; j < winner.length; j++)
+					winner[j] = -1;
+				tied = 0;
+			} else if (maxScore == score[i][0]) {
+				tied++;
+				winner[tied] = i;
+			}
+			
+		}
+
+		if (tied > 0) {
+			int maxNum = 0;
+			int[] tbWinner = new int[tied];
+			int tbTied = 0;
+			for (int i = 0; i < tied + 1; i++) {
+				if (maxNum < score[winner[i]][1]) {
+					maxNum = score[winner[i]][1];
+					tbWinner[0] = winner[i];
+					for (int j = 1; j < tbWinner.length; j++)
+						tbWinner[j] = 0;
+					tbTied = 0;
+				} else if (maxNum == score[winner[i]][1]) {
+					tbTied++;
+					tbWinner[tbTied] = i;
 				}
+			}
+			for (int i = 0; i < tbTied + 1; i++) {
+				winner[i] = tbWinner[i];
 			}
 		}
 	}
 
-	public static void arrayDoubleSort(int[][] score) {
-		for (int p = 0; p < score.length; p++) {
-			for (int i = 0; i < score[p].length - 1; i++) {
-				for (int j = i + 1; j < score[p].length; j++) {
-					if (score[p][i] < score[p][j]) {
-						int temp = score[p][i];
-						score[p][i] = score[p][j];
-						score[p][j] = temp;
+	public static void scoreClearing(int player, int[][] hands, int[][] score) {
+		if (royalJudge(player, hands) == 3 && flashJudge(player, hands)[0] == 4) {
+			score[player][0] = 900;
+		} else if (straightJudge(player, hands)[0] == 4 && flashJudge(player, hands)[0] == 4) {
+			score[player][0] = 800;
+		} else if (matchComboJudge(player, matchCount(player, hands))[0] == 700) {
+			score[player][0] = 700;
+		} else if (matchComboJudge(player, matchCount(player, hands))[0] == 600) {
+			score[player][0] = 600;
+		} else if (flashJudge(player, hands)[0] == 4) {
+			score[player][0] = 550;
+		} else if (straightJudge(player, hands)[0] == 4) {
+			score[player][0] = 500;
+		} else {
+			score[player][0] = matchComboJudge(player, matchCount(player, hands))[0];// 450,300,150
+		}
+	}
+
+	public static int royalJudge(int player, int[][] hands) {
+		int royal = 0;
+		if (hands[player][0] != 13 || hands[player][hands[player].length - 1] != 1) {
+			return royal;
+		} else {
+			for (int i = 1; i < hands[player].length - 1; i++) {
+				if (hands[player][i] == 12) {
+					if (hands[player][i - 1] > hands[player][i] + 1)
+						break;
+					if (hands[player][i - 1] == hands[player][i])
+						continue;
+					if (hands[player][i - 1] == hands[player][i] + 1) {
+						royal++;
+						if (royal == 4) {
+							return royal;
+						}
 					}
 				}
 			}
 		}
+		return royal;
 	}
 
-	public static void scoreJudge(int comboScore[][], int totalPersons) {
-		for (int i = 0; i < totalPersons; i++) {
-			scoreClearing(comboScore, i);
-		}
-		arrayLeftSort(comboScore);
-
-	}
-
-	public static void scoreClearing(int[][] hands, int[][] score, int p) {
-		arrayDoubleSort(hands);
-		if (royalJudge[0] == 4 && flashJudge[0] == 4)
-			score[p][0] = 900;
-		score[p][1] = royalJudge[1];// 使ったカードのindexだけ保存
-
-		if (straightJudge && flashJudge)
-			Score[p][0] = 800;
-//		if(forcard();)
-//			comboScore[p][0] =7;
-//		if(fullhouse();)
-//			comboScore[p][0] = 8;
-	}
-
-	public static int[] straightJudge(int nowPlayer, int[][] hands) {
-		arrayLeftSort(hands);
+	public static int[] straightJudge(int player, int[][] hands) {
 		int[] straight = new int[3]; // 0：成立判定 1:参照値 2:使用したインデックス
-		for (int i = 0; i < hands[nowPlayer].length - 4; i++) {
+		for (int i = 0; i < hands[player].length - 4; i++) {
 			straight[0] = 0;
 			straight[2] = 0;
-			for (int j = i + 1; j < hands[nowPlayer].length - 1; j++) {
-				if (hands[nowPlayer][j - 1] > hands[nowPlayer][j] + 1)
+			for (int j = i + 1; j < hands[player].length - 1; j++) {
+				if (hands[player][j - 1] > hands[player][j] + 1)
 					break;
-				if (hands[nowPlayer][j - 1] == hands[nowPlayer][j])
+				if (hands[player][j - 1] == hands[player][j])
 					continue;
-				if (hands[nowPlayer][j - 1] == hands[nowPlayer][j] + 1) {
+				if (hands[player][j - 1] == hands[player][j] + 1) {
 					straight[0]++;
 					straight[2] += convertDigit(j, straight[0]);
 					if (straight[0] == 4) {
@@ -316,13 +394,13 @@ public class G03_poker {
 		return straight;
 	}
 
-	public static int flashJudge(int nowPlayer, int[][] hands) {
+	public static int[] flashJudge(int player, int[][] hands) {
 
 		// フラッシュ判定
-		int flash = 0;
-		for (int i = 1; i < hands[nowPlayer].length; i++) {
-			if (hands[nowPlayer][0] / 13 == hands[nowPlayer][i] / 13) {
-				flash++;
+		int[] flash = new int[2];
+		for (int i = 1; i < hands[player].length; i++) {
+			if (hands[player][0] / 13 == hands[player][i] / 13) {
+				flash[0]++;
 			} else {
 				break;
 			}
@@ -330,15 +408,36 @@ public class G03_poker {
 		return flash;
 	}
 
-	public static int royal(int nowPlayer, int[][] hands) {
-		int royal = 0;
-		for (int i = 1; i < hands[nowPlayer].length; i++) {
-			if (hands[nowPlayer][0] % 13 == hands[nowPlayer][i] / 13) {
-				royal++;
-			} else {
+	public static int[] matchCount(int player, int[][] hands) {
+		int[] match = new int[hands[player].length];
+		for (int i = 0; i < hands[player].length; i++) {
+			for (int j = 0; j < hands[player].length; j++) {
+				if (hands[player][i] == hands[player][j]) {
+					match[i]++;
+				}
+			}
+		}
+		return match;
+	}
+
+	public static int[] matchComboJudge(int player, int[] match) {
+		int[] matchCombo = new int[2];
+		for (int i = 0; i < match.length; i++) {
+			switch (match[i]) {
+			case 4:
+				matchCombo[0] += 175;
+				break;
+			case 3:
+				matchCombo[0] += 150;
+				break;
+			case 2:
+				matchCombo[0] += 75;
+				break;
+			case 1:
+				matchCombo[0] += 0;
 				break;
 			}
 		}
-		return royal;
+		return matchCombo;
 	}
 }
